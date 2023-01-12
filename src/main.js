36039fcc1e2e31fe23dd5c66ec7e68bfca8a4ccd
Trevor Le <trevor.le1@ucalgary.ca>
2023-01-11 17:12:13 -0700

    Installed Vuetify

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 import { createPinia } from 'pinia'
 
-const pinia = createPinia()
-const app = createApp(App)
+loadFonts()
 
-app.use(pinia)
-app.mount('#app')
+createApp(App)
+  .use(store)
+  .use(vuetify)
+  .use(createPinia())
+  .mount('#app')
