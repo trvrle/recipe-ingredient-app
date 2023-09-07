35d92287dee2e68eaf7ef400a8b8a9599fdb189c
Trevor Le <trevor.le@altaml.com>
2023-09-06 22:44:17 -0600

    Add publicPath for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/recipe-ingredient-app/" : "/",
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+});
