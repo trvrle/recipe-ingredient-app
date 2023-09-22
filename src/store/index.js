261cddb7b657df90834fc01fdd153515efec31ec
Trevor Le <trevor.le@altaml.com>
2023-09-21 18:19:09 -0600

    Remove https agent

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,8 @@
 import { defineStore } from "pinia";
 import axios from "axios";
-import https from "https";
 
 const axiosInstance = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL,
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false,
-  }),
 });
 
 export const useRecipeStore = defineStore("recipe", {
